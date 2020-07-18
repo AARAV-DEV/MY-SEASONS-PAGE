3fcbe9c7e22c3bdf641ed6cc503068c3c9902e57
AARAV-DEV <67513131+AARAV-DEV@users.noreply.github.com>
2020-07-18 19:41:52 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function copy() {
+alert ("avoid laziness,try typing,rather copying");
+}
+function summer(){
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert ("WOO-HOO! its SUMMER");
+}
+function winter(){
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')"; 
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert ("its WINTER time")
+}
+function autumn(){
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert ("Its autumn y'all")
+}
+function spring(){
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.backgroundSize="100%";
+    alert ("SPRING'S here!")
+}
